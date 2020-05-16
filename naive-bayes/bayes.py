1d539f929a5c8b3e9715986176a6c740381352e1
Chotom <tomaszczochan@gmail.com>
2020-05-16 19:21:30 +0200

    Set test naive bayes to seprate file

--- naive-bayes/bayes.py
+++ naive-bayes/bayes.py
@@ -1,10 +1,14 @@
-import pandas as pd
-
-SPAM_PATH = "../data-collecting/data/processed/filter_uq_dataset1_spam.csv"
-HAM_PATH = "../data-collecting/data/processed/filter_uq_dataset1_ham.csv"
-
-
 def bayes(mail, df_spam, spam_count, df_ham, ham_count):
+    """
+
+    :param mail: string value with email text
+    :param df_spam: words from with counter in how many mail word appear
+    :param spam_count: All spam emails
+    :param df_ham: word from ham with counter in how many mail word appear
+    :param ham_count: All ham emails
+    :return: mail spam probability
+    """
+
     # Init values
     spam_probability = 1.0
     ham_probability = 1.0
@@ -14,49 +18,15 @@ def bayes(mail, df_spam, spam_count, df_ham, ham_count):
     for word in mail:
         # if word exist in either spam or ham database
         if word in df_spam.index or word in df_ham.index:
-            spam_probability *= df_spam['col_count'].loc[word]/spam_count
-            ham_probability *= df_ham['col_count'].loc[word]/ham_count
+            spam_probability *= df_spam['col_count'].loc[word] / spam_count
+            ham_probability *= df_ham['col_count'].loc[word] / ham_count
 
     estimated_count_spam = spam_probability * spam_count
     estimated_count_ham = ham_probability * ham_count
 
-    #print("estimated_count_spam: ", estimated_count_spam)
-    #print("estimated_count_ham: ", estimated_count_ham)
+    # print("estimated_count_spam: ", estimated_count_spam)
+    # print("estimated_count_ham: ", estimated_count_ham)
 
     # mail is spam with probability of
     probability = estimated_count_spam / (estimated_count_spam + estimated_count_ham)
-
     return probability
-
-
-def main():
-    # Load spam words with first column as index and second as value
-    df_spam = pd.read_csv(SPAM_PATH, index_col=0, names=['col_count'])
-    spam_count = df_spam[0:1]['col_count'].values[0]
-    df_spam = df_spam[1:]
-
-    # Load ham words with first column as index and second as value
-    df_ham = pd.read_csv(HAM_PATH, index_col=0, names=['col_count'])
-    ham_count = df_ham[0:1]['col_count'].values[0]
-    df_ham = df_ham[1:]
-
-    # testing
-    dataset_path = "../data-collecting/data/raw/spam_or_not_spam.csv"
-
-    header_names = ["content", "is_spam"]
-    mails_df = pd.read_csv(dataset_path, names=header_names, header=None).dropna()
-    print(mails_df)
-
-    counter = 0
-    for i in mails_df.index:
-        mail = mails_df.iloc[i]['content']
-        is_spam = mails_df.iloc[i]['is_spam']
-        p = bayes(mail, df_spam, spam_count, df_ham, ham_count)
-
-        if p > 0.5 and not is_spam:
-            counter += 1
-
-    print(counter)
-
-
-main()
---
+++ naive-bayes/test-naive-bayes.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from bayes import bayes
+
+SPAM_PATH = "../data-collecting/data/processed/filter_uq_dataset1_spam.csv"
+HAM_PATH = "../data-collecting/data/processed/filter_uq_dataset1_ham.csv"
+TEST_MAIL_PATH = "../data-collecting/data/raw/spam_or_not_spam.csv"
+
+
+def test_naive_bayes():
+    """
+
+    :return:
+    """
+    # Load mails to test
+    df_test_mails = pd.read_csv(TEST_MAIL_PATH, names=['content', 'is_spam'], header=None).dropna()
+
+    # Load spam words with first column as index and second as value
+    df_spam = pd.read_csv(SPAM_PATH, index_col=0, names=['col_count'])
+    spam_count = df_spam[0:1]['col_count'].values[0]
+    df_spam = df_spam[1:]
+
+    # Load ham words with first column as index and second as value
+    df_ham = pd.read_csv(HAM_PATH, index_col=0, names=['col_count'])
+    ham_count = df_ham[0:1]['col_count'].values[0]
+    df_ham = df_ham[1:]
+
+
+
+    #print(df_test_mails)
+    df_test_mails['spam_probability'] = df_test_mails['content'].apply(lambda mail: bayes(mail, df_spam, spam_count, df_ham, ham_count))
+
+    print(df_test_mails[['is_spam', 'spam_probability']])
+
+
+test_naive_bayes()
