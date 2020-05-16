7ba7354f541771ab6e34ba31f40c252e1711925f
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 21:24:27 +0200

    Change settings string

--- occurence_matrix/filter_mails.py
+++ occurence_matrix/filter_mails.py
@@ -5,7 +5,7 @@ from MailContentFilter import MailContentFilter
 def filter_mails():
     with open("settings.yaml", "r") as config_file:
         config = yaml.safe_load(config_file)
-        mail_content_filter_settings = config['MailContentFilter']
+        mail_content_filter_settings = config['filter_mails']
         raw_emails_file_path = mail_content_filter_settings['raw_emails_file_path']
         filtered_words_file_path = mail_content_filter_settings['filtered_words_file_path']
         filtered_mails_file_path = mail_content_filter_settings['filtered_mails_file_path']
