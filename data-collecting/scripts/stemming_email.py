08e3ba97d4799c1f909e6ed4631ab5a956564a91
Chotom <tomaszczochan@gmail.com>
2020-05-16 17:41:09 +0200

    Add string stemming function

--- data-collecting/scripts/stemming_email.py
+++ data-collecting/scripts/stemming_email.py
@@ -2,33 +2,35 @@ from nltk.stem import LancasterStemmer
 import pandas as pd
 
 
-def list_stemmer(word_list, stemmer):
+def string_stemmer(text):
+    """
+    :param text: words from email
+    :return: string with stemmed words
+    """
+    stemmer = LancasterStemmer()
+    stemmed_list = []
+
+    for word in text.split(' '):
+        stemmed_list.append(stemmer.stem(word))
+
+    return ' '.join(word for word in stemmed_list)
+
+
+def list_stemmer(word_list):
     """
     :param word_list: list of strings
-    :param stemmer: used stemmer from nltk.stem
     :return: list with stemmed words
     """
+    stemmer = LancasterStemmer()
     ret_list = []
+
     for word in word_list:
         ret_list.append(stemmer.stem(word))
 
     return ret_list
 
 
-def dataset_stemmer(df_dataset):
-    """
-    :param df_dataset: dataset with emails [email; is_spam]
-    :return: dataset with stemmed words
-    """
-    df_dataset["text"] = df_dataset["text"].apply(lambda text: text.split(' '))
-    df_dataset["text"] = df_dataset["text"].apply(lambda word_list: list_stemmer(word_list, stemmer))
-    df_dataset["text"] = df_dataset["text"].apply(lambda text: ' '.join(word for word in text))
-
-    return df_dataset
-
-
 # Init
-stemmer = LancasterStemmer()
 PATH_DATASET1 = "../data/raw/org_spamham.csv"
 PATH_DATASET2 = "../data/raw/org_spam_or_not_spam.csv"
 PATH_SAVE_DATASET1 = "../data/raw/spamham.csv"
@@ -40,8 +42,8 @@ df_dataset1 = pd.read_csv(PATH_DATASET1, names=HEADER_NAMES, header=None).dropna
 df_dataset2 = pd.read_csv(PATH_DATASET2, names=HEADER_NAMES, header=None).dropna()
 
 # Preprocessed data in email
-df_dataset1 = dataset_stemmer(df_dataset1)
-df_dataset2 = dataset_stemmer(df_dataset2)
+df_dataset1["text"] = df_dataset1["text"].apply(lambda text: string_stemmer(text))
+df_dataset2["text"] = df_dataset2["text"].apply(lambda text: string_stemmer(text))
 
 # Save
 df_dataset1.to_csv(PATH_SAVE_DATASET1, header=False, index=False)
