2aa074f333574387191a31af61e2212af9c74780
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 21:42:35 +0200

    Remove unnecessary prints

--- occurence_matrix/build_word_matrix_for_mail.py
+++ occurence_matrix/build_word_matrix_for_mail.py
@@ -12,16 +12,13 @@ def build_word_matrix_for_mail(filtered_words_set_file_path, mail_file_path,
     mail_filter.load_filtered_words_set(filtered_words_set_file_path, ['word'])
     with open(mail_file_path, 'r') as file:
         mail = file.read().replace('\n', '')
-    print(list(mail_filter.filtered_words_set))
     mail = mail_filter.filter_mail(mail)
-    #print(mail)
     matrix_builder = OccurrenceMatrixBuilder()
     matrix_builder.load_words_list(filtered_words_set_file_path)
     words_matrix = matrix_builder.create_words_matrix()
     matrix_builder.set_correlation_distance_explicitly(correlation_distance)
     matrix_builder.read_mail_to_words_matrix(words_matrix, mail)
     mail_words_matrix_df = matrix_builder.create_data_frame_from_words_matrix(words_matrix)
-    #print(mail_words_matrix_df)
     matrix_builder.save_words_matrix_to_csv(mail_words_matrix_df, mail_matrix_output_file_path)
 
 
