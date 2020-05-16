760eceab7c2016a1dfd4cb8f572c32c52ca3c3af
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 21:41:22 +0200

    Fix minor bugs in build_word_matrix_for_mail script

--- occurence_matrix/build_word_matrix_for_mail.py
+++ occurence_matrix/build_word_matrix_for_mail.py
@@ -1,24 +1,28 @@
 import yaml
-from OccurenceMatrixBuilder import OccurenceMatrixBuilder
+from OccurrenceMatrixBuilder import OccurrenceMatrixBuilder
 from MailContentFilter import MailContentFilter
 
 
-def build_word_matrix_for_mail(filtered_words_set_file_path, mail_file_path, mail_matrix_output_file_path):
+def build_word_matrix_for_mail(filtered_words_set_file_path, mail_file_path,
+                               mail_matrix_output_file_path, correlation_distance):
     with open(mail_file_path, 'r') as file:
         mail_content = file.read().replace('\n', '')
 
     mail_filter = MailContentFilter()
-    mail_filter.load_filtered_words_set(filtered_words_set_file_path)
+    mail_filter.load_filtered_words_set(filtered_words_set_file_path, ['word'])
     with open(mail_file_path, 'r') as file:
         mail = file.read().replace('\n', '')
+    print(list(mail_filter.filtered_words_set))
     mail = mail_filter.filter_mail(mail)
-
-    matrix_builder = OccurenceMatrixBuilder()
+    #print(mail)
+    matrix_builder = OccurrenceMatrixBuilder()
     matrix_builder.load_words_list(filtered_words_set_file_path)
     words_matrix = matrix_builder.create_words_matrix()
+    matrix_builder.set_correlation_distance_explicitly(correlation_distance)
     matrix_builder.read_mail_to_words_matrix(words_matrix, mail)
-    matrix_builder.create_data_frame_from_words_matrix(words_matrix)
-    matrix_builder.save_words_matrix_to_csv(mail_matrix_output_file_path)
+    mail_words_matrix_df = matrix_builder.create_data_frame_from_words_matrix(words_matrix)
+    #print(mail_words_matrix_df)
+    matrix_builder.save_words_matrix_to_csv(mail_words_matrix_df, mail_matrix_output_file_path)
 
 
 if __name__ == "__main__":
@@ -28,4 +32,5 @@ if __name__ == "__main__":
         filtered_words_set_file_path = mail_content_filter_settings['filtered_words_set_file_path']
         mail_file_path = mail_content_filter_settings['mail_file_path']
         mail_matrix_output_file_path = mail_content_filter_settings['mail_matrix_output_file_path']
-    build_word_matrix_for_mail(filtered_words_set_file_path, mail_file_path, mail_matrix_output_file_path)
+        correlation_distance = mail_content_filter_settings['correlation_distance']
+    build_word_matrix_for_mail(filtered_words_set_file_path, mail_file_path, mail_matrix_output_file_path, correlation_distance)
